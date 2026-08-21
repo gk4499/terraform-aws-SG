89714f617f08c0848f35ef46a969ccf0fae0aa9c
Gokula Krishnan J <gkrishnan4499@gmail.com>
2026-08-21 16:05:46 +0530

    Correct typo in security group output
    Fix typo in aws_security_group reference.

--- modules/terraform-aws-sg/outputs.tf
+++ modules/terraform-aws-sg/outputs.tf
@@ -2,7 +2,7 @@ output "resources" {
   description = "Managed resources"
 
   value = {
-    aws_security_group                     = aws_sercurity_group.current,
+    aws_security_group                     = aws_security_group.current,
     aws_vpc_security_group_engress_rule    = aws_vpc_security_group_egress_rule.current,
     aws_vpc_security_group_ingress_rule    = aws_vpc_security_group_ingress_rule.current,
     aws_vpc_security_group_rules_exclusive = aws_vpc_security_group_rules_exclusive.current,
